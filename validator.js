e192bd61f838f3ecba80b38d0bb8b6c54d411a4a
nirmal bhagwani <nil4u.you@gmail.com>
2014-10-28 17:55:50 +0530

    extending literal usage for better performance

--- validator.js
+++ validator.js
@@ -11,27 +11,35 @@ var validdata = {
 	minlen : 10,
 	minchar : 4
 };
+			
 var valid =   (function (iv,dv){
 	 var private_valid = function() {
-		$(validdata.Formid + ' .valid').each(function(index, element) {//calling validation for every field
+		 $(validdata.Formid + ' div.invalid-form-field').remove();
+		 $(validdata.Formid + ' input').removeClass('invalid-field');
+		$(validdata.Formid + ' .valid[type="text"]').each(function(index, element) {//calling validation for every field
 			 var dvalue = $(this).attr('value');
 			 var ivalue = $(this).val();
+
 				 if($(this).hasClass('mail')){//calling mail validation
-					validator = valid.mail(ivalue);
+					validator = valid.mail(ivalue,this);
 				 }
 				 else if($(this).hasClass('phone')){
-					 validator = valid.phnnum(ivalue.length);
+					 validator = valid.phnnum(ivalue.length,this);
+					 if(validator === false || validator == false){
+					 }
 				 }
 				 else{
-					validator = valid.check(ivalue,dvalue,ivalue.length);
-					return validator;
+					validator = valid.check(ivalue,dvalue,ivalue.length,this);
+					//alert(validator == false);
+					if(validator === false || validator == false){
+						 this.focus();
+						 if ($(this).val()==dvalue){
+							 $(this).val("");
+						 	 return validator;
+						 }
+						return validator;
+					 }
 				 }
-			 if(validator === false || validator == false){
-				  this.focus();
-				 if ($(this).val()==dvalue)
-					this.val="";
-				  return validator;
-			 }
 		  });
 		  if(validator === false || validator == false){
 			  return false;
@@ -44,14 +52,41 @@ var valid =   (function (iv,dv){
 		validatin: function(){
 			return private_valid();
 		},
-		check: function(iv,dv,le){//check non empty field
+		init:function(e){
+			$(validdata.Formid+ ' input[type="text"]').on('focus',function(e){
+				var a = $(this).attr('value');
+				var b = $(this).val();
+				if(a == b || a ==""){
+					$(this).val("");
+				}
+			});
+			$(validdata.Formid+ " input").on('blur',function(e){
+				var a = $(this).attr('value');
+				var b = $(this).val();
+				if(a == b || b ==""){
+					$(this).val(a);
+				}
+			});
+			$(validdata.Formid).on('submit',function(e){
+				valid.validatin();
+				return false;
+			});
+			$('input[name="pageURL"]').val(document.URL);
+			$('input.numonly').keypress(function(e) {
+                return valid.numonly(e.keyCode);
+            });
+		},
+		check: function(iv,dv,le,el){//check non empty field
 			if(iv == dv || iv ==""){
-				alert('Please Fill ' + dv);
+				$(el).addClass('invalid-field').after('<div class="invalid-form-field">Please Fill '+dv+'</div>');
+//				alert('Please Fill ' + dv);
+				$(this).val("");
 				return false;
 			}
 			else{
 				if(le < validdata.minchar){
-				alert("Length to sort for "+dv);
+				$(el).addClass('invalid-field').after('<div class="invalid-form-field">Length to sort for '+dv+'</div>');
+				//alert("Length to sort for "+dv);
 				return  false;
 				}
 				else{
@@ -67,21 +102,23 @@ var valid =   (function (iv,dv){
 				return true;
 			}
 		},
-		mail: function(mval){//check if mail id
+		mail: function(mval,el){//check if mail id
 			var atpos=mval.indexOf("@");
 			var dotpos=mval.lastIndexOf(".");
 			if (atpos<1 || dotpos<atpos+2 || dotpos+2>=mval.length)
 			  {
-				  alert("Not a valid e-mail address");
+				  $(el).addClass('invalid-field').after('<div class="invalid-form-field">Invalid Email Address</div>');
+				 // alert("Not a valid e-mail address");
 				  return false;
 			  }
 			 else{
 			 	 return true;
 			 }
 		},
-		phnnum: function(len){
+		phnnum: function(len,el){
 			if (len < validdata.minlen || len > validdata.maxlen) {
-				alert('Please inser proper length phone number');
+				$(el).addClass('invalid-field').after('<div class="invalid-form-field">Phone Number too Sort.</div>');
+			//	alert('Please inser proper length phone number');
 				return false ;
 			}
 			 else{
